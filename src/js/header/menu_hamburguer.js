b160eedf81cd95a5034e3ce8569ceb15fbcfafa0
Vigilio <yonatanvigiliolavado09@gmail.com>
2022-02-03 20:36:41 -0500

    responsive header wallpaper

---
+++ src/js/header/menu_hamburguer.js
@@ -0,0 +1,27 @@
+const menuHamburguer = document.querySelector('#menu-hamburguer');
+const navbarMenu = document.querySelector('.navbar-menu');
+const navbarSearch = document.querySelector('.navbar-search');
+const distritoChuquis = document.querySelector('#distritoChuquis');
+const distritoChuquisMenu = document.querySelector('.navbar-menu-list-submenu');
+menuHamburguer.addEventListener('click', ()=>{
+    navbarMenu.classList.toggle('navbar-menu-actived');
+    navbarSearch.classList.toggle('navbar-search-actived');
+})
+
+distritoChuquis.addEventListener('click', ()=>{
+
+ 
+    if(!distritoChuquisMenu.classList.contains('navbar-menu-list-submenu-actived')){
+        distritoChuquisMenu.classList.add('navbar-menu-list-submenu-actived');
+        distritoChuquisMenu.style.display = 'block';
+    }else{
+        distritoChuquisMenu.classList.remove('navbar-menu-list-submenu-actived');
+        distritoChuquisMenu.style.display = 'none';  
+    }
+})
+
+// distritoChuquis.addEventListener('mouseover', ()=>{
+//     
+    
+// })
+
